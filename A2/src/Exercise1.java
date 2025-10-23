f86d2c6e30f9c64d66d22e6a1bd5c88055fc4b1d
Antony Punnassery <114343761+Hyperloop360@users.noreply.github.com>
2025-10-22 21:25:14 -0300

    A2

---
+++ A2/src/Exercise1.java
@@ -0,0 +1,23 @@
+/**
+ * Antony Punnassery
+ * B01012905
+ */
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Positive Integer:");
+        int n =  sc.nextInt();
+
+        long startTime, endTime, executionTime;
+        startTime=System.currentTimeMillis();
+
+        endTime=System.currentTimeMillis();
+        executionTime = endTime - startTime;
+
+    }
+
+
+
+}
---
+++ A2/src/Exercise2.java
@@ -0,0 +1,15 @@
+/**
+ * Antony Punnassery
+ * B01012905
+ */
+
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
