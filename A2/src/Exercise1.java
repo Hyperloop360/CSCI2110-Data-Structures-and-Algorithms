4aad1b8bf26bcecf0007f7ae37108ba1d1648a27
antonyPunnassery <antony2006.punnassery@gmail.com>
2025-10-22 22:44:05 -0300

    Finished A2

--- A2/src/Exercise1.java
+++ A2/src/Exercise1.java
@@ -7,17 +7,42 @@ import java.util.Scanner;
 public class Exercise1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a Positive Integer:");
-        int n =  sc.nextInt();
+        while (sc.hasNext()) {
+            long n = sc.nextLong();
+            if (n == 0) break;
 
-        long startTime, endTime, executionTime;
-        startTime=System.currentTimeMillis();
+            long startTime = System.currentTimeMillis();
+            long bestNumber = 1;
+            long maxLength = 1;
 
-        endTime=System.currentTimeMillis();
-        executionTime = endTime - startTime;
+            // Check all numbers from 1 to n
+            for (long i = 1; i <= n; i++) {
+                long current = i;
+                long length = 1;
+
+                // Collatz sequence
+                while (current != 1) {
+                    if (current % 2 == 0) {
+                        current = current / 2;
+                    } else {
+                        current = 3 * current + 1;
+                    }
+                    length++;
+                }
+
+                // Update if this sequence is longer
+                if (length > maxLength) {
+                    maxLength = length;
+                    bestNumber = i;
+                }
+            }
+
+            long endTime = System.currentTimeMillis();
+            long time = endTime - startTime;
+
+            // Output: n bestNumber maxLength time
+            System.out.println(n + " " + bestNumber + " " + maxLength + " " + time);
+        }
 
     }
-
-
-
 }
--- A2/src/Exercise2.java
+++ A2/src/Exercise2.java
@@ -7,9 +7,44 @@ import java.util.Scanner;
 
 public class Exercise2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        while (scanner.hasNext()) {
+            long n = scanner.nextLong();
+            if (n == 0) break;
 
+            long startTime = System.currentTimeMillis();
+            long bestNumber = 1;
+            long maxLength = 1;
 
+            for (long i = 1; i <= n; i++) {
+                long current = i;
+                long length = 1;
 
+                while (current != 1) {
+                    if (current % 2 == 0) {
+                        current = current / 2;
+                    } else {
+                        if (current % 4 == 1) {
+                            current = 7 * current + 1;
+                        } else {
+                            current = 7 * current - 1;
+                        }
+                    }
+                    length++;
+                }
+
+                // Update if this sequence is longer
+                if (length > maxLength) {
+                    maxLength = length;
+                    bestNumber = i;
+                }
+            }
+
+            long endTime = System.currentTimeMillis();
+            long time = endTime - startTime;
+
+            System.out.println(n + " " + bestNumber + " " + maxLength + " " + time);
+        }
     }
 }
